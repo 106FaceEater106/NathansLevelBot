491c19ce1a8c88fb040c709d23209c94245db7fe
Facing-South <Paulwenzlaff@gmail.com>
2020-08-26 15:48:06 +0200

    Project created

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+
+namespace NathansLevelBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MouseOperations.SetCursorPosition(10, 1050);
+            MouseOperations.MouseEvent(MouseOperations.MouseEventFlags.LeftDown);
+            MouseOperations.MouseEvent(MouseOperations.MouseEventFlags.LeftUp);
+            Thread.Sleep(2500);
+        }
+    }
+}
